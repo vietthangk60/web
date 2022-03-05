4be5abeb24224c0f158dcc829bb242bf30fd3a0f
DESKTOP-47FO79J\HORUS <vietthangk54@gmail.com>
2022-03-05 14:45:16 +0700

    5_3_2022

--- Main_Web/Main_Nhansu/views.py
+++ Main_Web/Main_Nhansu/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-
+from Main_ThemnhanVien.models import Employees
+from Main_ThemnhanVien.serializers import EmployeeSerializer
 # Create your views here.
 def func_Nhansuview(request, *args, **kwargs): # *args, **kwargs
    # print(args, kwargs)
    # print(request.user)
     #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
-    return render(request, "nhansu.html", {})
+   if request.method=='GET':
+      employees = Employees.objects.all()
+      employees_serializer=EmployeeSerializer(employees,many=True)
+      return render(request, "nhansu.html", {"employee":employees_serializer.data})
+   elif request.method=='POST' and 'delete' in request.POST:
+      print(request.POST)
+      manhavien=request.POST['delete']
+      record = Employees.objects.get(EmployeeId=manhavien)
+      record.delete()
+      employees = Employees.objects.all()
+      employees_serializer=EmployeeSerializer(employees,many=True)
+      return render(request, "nhansu.html", {"employee":employees_serializer.data})
 def func_Themnhanvienview(request, *args, **kwargs): # *args, **kwargs
    # print(args, kwargs)
    # print(request.user)
--- Main_Web/Main_ThemnhanVien/views.py
+++ Main_Web/Main_ThemnhanVien/views.py
@@ -12,36 +12,65 @@ from django.core.files.storage import default_storage
 
 
 @csrf_exempt
-def employeeApi(request,id=0):
+def employeeApi(request):
     if request.method=='GET':
         employees = Employees.objects.all()
         employees_serializer=EmployeeSerializer(employees,many=True)
         return render(request, "themnhanvien.html", {})
         #return JsonResponse(employees_serializer.data,safe=False)
     elif request.method=='POST':
-        employee_data=JSONParser().parse(request)
+        #employee_data=JSONParser().parse(request)
        # serializer = EmployeeSerializer(data=request.data)
       #  a=request.POST['EmployeeName'] 
-        print("da chay fffsfsfs",employee_data)
-        employees_serializer=EmployeeSerializer(data=employee_data)
+       # print("da chay fffsfsfs",employee_data)
+       # employees_serializer=EmployeeSerializer(data=employee_data)
         
-        if employees_serializer.is_valid():
-            employees_serializer.save()
-            return JsonResponse("Added Successfully",safe=False)
-        print(employees_serializer.errors)
-        return JsonResponse("Failed to Add",safe=False)
-    elif request.method=='PUT':
-        employee_data=JSONParser().parse(request)
-        employee=Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
-        employees_serializer=EmployeeSerializer(employee,data=employee_data)
-        if employees_serializer.is_valid():
-            employees_serializer.save()
-            return JsonResponse("Updated Successfully",safe=False)
-        return JsonResponse("Failed to Update")
-    elif request.method=='DELETE':
-        employee=Employees.objects.get(EmployeeId=id)
-        employee.delete()
-        return JsonResponse("Deleted Successfully",safe=False)
+        #if employees_serializer.is_valid():
+           # employees_serializer.save()
+           # return JsonResponse("Added Successfully",safe=False)
+       # print(employees_serializer.errors)
+       # return JsonResponse("Failed to Add",safe=False)
+      #  myfile = request.FILES['avata']    # lấy file từ Input Image quá Post
+        myfile = request.FILES['avata'] 
+        bophan={"1": "Dev","2": "Game design","3": "Art","4":"Tester"}
+        Chuvu ={"1":"Nhân viên","2":"Leader","3":"Quản lý","4":"Giám đốc"}
+      #  id_NV =request.POST.get('inputmnv')
+        InputTen = request.POST.get('inputname')
+        InputSoDienThoai = request.POST.get('inputTel')
+        InputEmail = request.POST.get('inputEmail')
+        InputBirthDate = request.POST.get('inputdate')
+        inpurAddress=request.POST.get('inputdate')
+        InputTeam = request.POST.get('inputbophan')
+        Inputchucvu= request.POST.get('inputchucdanh')
+        inputdiachi=request.POST.get('diachi')
+
+
+       # InputAvatar= myfile.name
+       # print(InputTen,InputSoDienThoai,InputEmail,InputBirthDate)
+        print(Chuvu[str(Inputchucvu)],inputdiachi)
+        employees = Employees.objects.create(EmployeeName=InputTen,Date_of_birth=InputBirthDate,PhoneNumber=InputSoDienThoai,Address_Employee=inputdiachi,Department=bophan[str(InputTeam)],Position_Employee=Chuvu[str(Inputchucvu)],Email=InputEmail,Avatar=myfile)
+
+       # print(employees)
+        employees.save()
+
+        employees = Employees.objects.all()
+        employees_serializer=EmployeeSerializer(employees,many=True)
+
+        return render(request, "nhansu.html", {"employee":employees_serializer.data})
+
+
+  #  elif request.method=='PUT':
+  #      employee_data=JSONParser().parse(request)
+ #       employee=Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
+ #       employees_serializer=EmployeeSerializer(employee,data=employee_data)
+ #       if employees_serializer.is_valid():
+  #          employees_serializer.save()
+   #         return JsonResponse("Updated Successfully",safe=False)
+  #      return JsonResponse("Failed to Update")
+  #  elif request.method=='DELETE':
+  #      employee=Employees.objects.get(EmployeeId=id)
+ #       employee.delete()
+  #      return JsonResponse("Deleted Successfully",safe=False)
 
 #def register_attempt(request):
 
