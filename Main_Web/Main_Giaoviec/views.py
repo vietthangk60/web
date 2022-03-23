b48c9f13b871fb5328d667d71f67ea247cbcf5e4
dangbnhorus <dangbn@horusent.com>
2022-03-23 18:49:23 +0700

    themduan

--- Main_Web/Main_Giaoviec/views.py
+++ Main_Web/Main_Giaoviec/views.py
@@ -14,3 +14,11 @@ def func_DuanView(request, *args, **kwargs): # *args, **kwargs
     #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
     
     return render(request, "duan.html", {})
+
+
+def func_ThemduanView(request, *args, **kwargs):  # *args, **kwargs
+   # print(args, kwargs)
+   # print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+
+    return render(request, "themduan.html", {})
